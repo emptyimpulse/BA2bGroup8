2b5271858fb2df97e4ed7dcc61e4c626aec2db97
emptyimpulse <3mptyimpulse@gmail.com>
2025-03-17 15:21:29 +0000

    Project Upload w/ C++

---
+++ Source/JumpingToConclusions/JumpingToConclusionsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JumpingToConclusionsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AJumpingToConclusionsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AJumpingToConclusionsGameMode();
+};
+
+
+
